719215ed73ab66d51e9e22b7972cca2bed16e4c7
flo <you@example.com>
2019-03-03 15:45:01 +0100

    added hamming exercise submission

---
+++ hamming/hamming.go
@@ -0,0 +1,19 @@
+package hamming
+
+import "errors"
+
+/*
+Distance comparing two DNA strands and counting how many of the nucleotides are different from their equivalent in the other string.
+*/
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, errors.New("the Hamming distance is only defined for sequences of equal length")
+	}
+	sum := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			sum++
+		}
+	}
+	return sum, nil
+}
