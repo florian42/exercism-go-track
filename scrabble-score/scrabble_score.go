76c24d5eb47a1af86fca8ba52ab186869f28d43d
flo <you@example.com>
2019-04-03 19:44:26 +0200

    improved scrabble-score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -3,48 +3,32 @@ Package scrabble implements a function to calculate the scrabble score for a giv
 */
 package scrabble
 
-import "strings"
-
-var letterValues = map[string]int{
-	"A": 1,
-	"E": 1,
-	"I": 1,
-	"O": 1,
-	"U": 1,
-	"L": 1,
-	"N": 1,
-	"R": 1,
-	"S": 1,
-	"T": 1,
-	"D": 2,
-	"G": 2,
-	"B": 3,
-	"C": 3,
-	"M": 3,
-	"P": 3,
-	"F": 4,
-	"H": 4,
-	"V": 4,
-	"W": 4,
-	"Y": 4,
-	"K": 5,
-	"J": 8,
-	"X": 8,
-	"Q": 10,
-	"Z": 10,
-}
+import "unicode"
 
 /*
 Score given a word, computes the scrabble score for that word.
 */
 func Score(input string) int {
-	if input == "" {
-		return 0
-	}
-	upperCaseInput := strings.ToUpper(input)
 	sum := 0
-	for _, char := range upperCaseInput {
-		sum += letterValues[string(char)]
+	for _, char := range input {
+		switch unicode.ToUpper(char) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			sum++
+		case 'D', 'G':
+			sum += 2
+		case 'B', 'C', 'M', 'P':
+			sum += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			sum += 4
+		case 'K':
+			sum += 5
+		case 'J', 'X':
+			sum += 8
+		case 'Q', 'Z':
+			sum += 10
+		default:
+			sum += 0
+		}
 	}
 	return sum
 }
